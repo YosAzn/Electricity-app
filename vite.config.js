760d6926b65aeefe83e6ae5fc667b7cb31581c3d
יוסף אזן <yosefazan@tauex.tau.ac.il>
2026-04-25 02:43:36 +0300

    Fixed base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 
-  base: '/Electricity-app/', // <--- השורה שצריך להוסיף (עם הלוכסנים)
+  base: '/electricity-app/', // <--- השורה שצריך להוסיף (עם הלוכסנים)
 
   plugins: [react()],
 
